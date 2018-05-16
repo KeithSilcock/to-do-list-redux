14d85cc794f0d635e541a75c900cad97a67f9c3c
KeithSilcock <silcockk@gmail.com>
2018-05-16 12:16:22 -0700

    added detail page for items added a button to go back to full list

--- src/components/single_item.js
+++ src/components/single_item.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {getSingleItem} from '../actions'
 
 class SingleItem extends React.Component{
 
     componentDidMount(){
-        console.log(this.props)
-        
+        this.props.getItem(this.props.match.params.id)
     }
 
     render(){
+        const {title, details} = this.props.item
+        console.log(this.props)
         return(
-            <div>
-                <h1>Your mom's a single item</h1>
+            <div className="center">
+                <h1>To Do Item</h1>
+                <div className="row right-align">
+                    <Link to='/' className='btn green darken-3'>Back to Full List</Link>
+                </div>
+                <h3>{title}</h3>
+                <h4>{details}</h4>
             </div>
         )
     }
 }
 
-export default SingleItem;
+function mapStateToProps(state){
+    return {
+        item: state.list.single
+    }
+}
+
+export default connect(mapStateToProps, {getItem: getSingleItem})(SingleItem);
